bba1775b558630db6e6fb2fe68d72151a9120649
Courtney Ross <cross52@student.gsu.edu>
2025-11-10 18:54:43 -0500

    Load and concatenate spam training datasets

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix
+
+
+df1 = pd.read_csv("spam_train1.csv")
+df2 = pd.read_csv("spam_train2.csv")
+df = pd.concat([df1, df2])
+print(df)
