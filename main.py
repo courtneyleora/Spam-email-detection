14158f465253492aa544f8cf463bfdb88f90773f
Courtney Ross <cross52@student.gsu.edu>
2025-11-10 18:24:17 -0500

    Remove unused imports and clean up code structure

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import sys
-import string
-import nltk
-from nltk.corpus import stopwords
-from wordcloud import WordCloud
-nltk.download('stopwords')
-
-import tensorflow as tf
-from keras_preprocessing.text import Tokenizer
-from keras_preprocessing.sequence import pad_sequences
 from sklearn.model_selection import train_test_split
-from keras.callbacks import EarlyStopping, ReduceLROnPlateau
-
-import warnings
-warnings.filterwarnings('ignore')
-
-
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
